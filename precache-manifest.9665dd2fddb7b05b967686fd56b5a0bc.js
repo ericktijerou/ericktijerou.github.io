3efa83193f7a30f0afee29c0087d8c361d51cde9
Erick Tijero <erick@kanto.co>
2021-09-08 21:31:04 -0500

    Updates

---
+++ precache-manifest.9665dd2fddb7b05b967686fd56b5a0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b4a66afd1d8e404f550e18def614c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed22fd53b2467b2e1c6e",
+    "url": "/static/css/main.af438b42.chunk.css"
+  },
+  {
+    "revision": "6dc9f49a2fe7cde12953",
+    "url": "/static/js/2.5ce78385.chunk.js"
+  },
+  {
+    "revision": "d728072c49727aef190d46667c1621ae",
+    "url": "/static/js/2.5ce78385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed22fd53b2467b2e1c6e",
+    "url": "/static/js/main.ab452c29.chunk.js"
+  },
+  {
+    "revision": "0827c6ccee7085c5e93e",
+    "url": "/static/js/runtime-main.3e36238a.js"
+  }
+]);
